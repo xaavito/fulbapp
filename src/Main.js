b89fe32c52c7f9790f9e1ad25e4cc104275af3da
xaavito <jgonzalez@resolvit.com>
2019-03-26 21:19:05 -0300

    Un poco de styling

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import mainPhoto from '../src/images/main.jpg';
+import '../src/resources/style.scss';
 
 class Main extends Component {
     render() {
@@ -26,21 +27,24 @@ class Main extends Component {
         }
         return (
             <div>
-                <img src={mainPhoto} alt="Main Foto"/>
-                <h1>Bienvenido Dani!</h1>
-                <ul className="header">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/stuff">Stuff</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
+                <img src={mainPhoto} alt="Main Foto" className="image-full" />
+                <h1 className="main-title">Sistema de confirmacion al partido de los miercoles</h1>
                 <div className="content">
-                    <select>
-                        <option value="C">Confirmo</option>
-                        <option value="B">Baja</option>
-                        <option value="S">Suplente</option>
-                    </select>
+                    <h1>Bienvenido Dani!</h1>
+                    <ul className="header">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/stuff">Stuff</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                    <div className="select">
+                        <select>
+                            <option value="C">Confirmo</option>
+                            <option value="B">Baja</option>
+                            <option value="S">Suplente</option>
+                        </select>
+                    </div>
+                    <button className="main-button" type="button" onClick={() => confirmarAlDoparti()}>Confirmar</button>
                 </div>
-                <button type="button" onClick={() => confirmarAlDoparti()}>Confirmar</button>
             </div >
         );
     }

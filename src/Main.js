61f9ac9488dc5006516557eb15e0c646b233c3a8
xaavito <jgonzalez@resolvit.com>
2019-03-29 15:24:57 -0300

    favicon added

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,17 @@
 
 import React, { Component } from "react";
-import { BrowserRouter, Route , Switch} from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Confirmacion from './Confirmacion/Confirmacion'
 import CrearPartido from './CrearPartido/CrearPartido'
 
 class Main extends Component {
+
     render() {
+        var http = require("http");
+        setInterval(function () {
+            http.get("https://fulbapp-cli.herokuapp.com/");
+        }, 300000); // every 5 minutes (300000)
+
         return (
             <BrowserRouter>
                 <Switch>

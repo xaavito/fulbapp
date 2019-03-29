893dae26cb63bce637108cd6bc26ed0f19b95d84
xaavito <jgonzalez@resolvit.com>
2019-03-29 13:23:48 -0300

    Adding new relic

--- src/CrearPartido/CrearPartido.js
+++ src/CrearPartido/CrearPartido.js
@@ -25,7 +25,7 @@ class CrearPartido extends Component {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ 'fecha': fechaFormateada })
+                body: JSON.stringify({ "fecha": fechaFormateada })
             }).then((response) => {
                 if (!response.ok) {
                     throw new Error(response.status);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
+require('newrelic');
  
 ReactDOM.render(
   <Main/>, 

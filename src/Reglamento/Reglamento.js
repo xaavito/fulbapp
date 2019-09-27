e55fffc097c8d836b620af2c9296d699d3dac7d0
Javier Gonzalez <javiermartingonzalez@gmail.com>
2019-09-26 21:23:21 -0300

    Nueva pagina de reglamento

---
+++ src/Reglamento/Reglamento.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import '../../src/resources/style.scss';
+import Table from 'react-bootstrap/Table'
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+
+class Reglamento extends Component {
+    API_ENDPOINT = 'https://fulbapp-serv.herokuapp.com';
+
+    render() {
+        return (
+            <div className="main-content">
+                <div className="table-content-asistentes" >
+                    <h1 className="main-title">REGLAMENTO</h1>
+                    <Table borderlessvariant="dark">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Mail</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr key="1">
+                                <td>2</td>
+                                <td>3</td>
+                                <td>4</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+            </div >
+        );
+    }
+}
+
+export default Reglamento;
---
+++ src/Reglamento/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Reglamento from "./Reglamento";
+ 
+ReactDOM.render(
+  <Reglamento/>, 
+  document.getElementById("root")
+);
+
+export default Reglamento
